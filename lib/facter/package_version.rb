e422c4b069f2d784c5a727c50ee9cb20458c19a4
Gary Larizza <gary@puppetlabs.com>
2017-03-30 11:30:50 -0700

    New 'package_version' fact
    This fact will use the RAL to query for a package and return its version (or
    nothing). This fact is dependent on the OS and the OS's default package
    provider, and was written for Linux.

---
+++ lib/facter/package_version.rb
@@ -0,0 +1,22 @@
+###################
+# package_version #
+###################  
+#  
+# This fact will use the Puppet RAL to query whether a package has been
+# installed (using the default package provider). If it is, the version string
+# that Puppet is aware of will be reported. If not, this fact will be absent.
+# 
+# To specify the name of the package you're looking for, just change the
+# "package" variable below from 'bash' to the package name as known by the
+# default package provider.
+#
+Facter.add(:centrify) do
+  setcode do
+    package  = 'bash'
+    instance = Puppet::Type.type('package').instances.select { |pkg| pkg.name == package }.first
+    if instance
+      ensure_property = instance.property(:ensure)
+      instance.retrieve[ensure_property]
+    end
+  end
+end
